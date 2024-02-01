0643c32eeda287cadb5f9ffa73b3cd24667fa10d
jpdatascientist <stqy77@gmail.com>
2024-02-01 22:39:36 +0900

    first

---
+++ Assets/Aircraft/Script/AircraftAgent.cs
@@ -0,0 +1,226 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using UnityEngine;
+using Unity.MLAgents.Policies;
+
+
+namespace Aircraft
+{
+    public class AircraftAgent : Agent
+    {
+        [Header("Movement Parameters")]
+        public float thrust = 100000f; //Aircraftを前に進ませるスピード　Z軸
+        public float pitchSpeed = 100f;//羽も動き、傾きなど
+        public float yawSpeed = 100f;
+        public float rollSpeed = 100f;
+        public float boostMultiplier = 2f;
+
+        [Header("Explosion Stuff")]
+        [Tooltip("The aircraft mesh that will disappear on explosion")]
+        public GameObject meshObject;
+
+        [Tooltip("The game object of the explosion particle effect")]
+        public GameObject explosionEffect;
+
+        [Header("Training")]
+        [Tooltip("Number of steps to time out after in training")]
+        public int stepTimeout = 300;
+
+
+
+        public int NextCheckpointIndex { get; set; }
+
+        private AircraftArea area;
+        new private Rigidbody rigidbody;
+        private TrailRenderer trail;
+
+
+        // When the next step timeout will be during training
+        private float nextStepTimeout;
+
+        // Whether the aircraft is frozen (intentionally not flying)
+        private bool frozen = false;
+
+
+
+        // Controls
+        private float pitchChange = 0f;
+        private float smoothPitchChange = 0f;
+        private float maxPitchAngle = 45f;
+        private float yawChange = 0f;
+        private float smoothYawChange = 0f;
+        private float rollChange = 0f;
+        private float smoothRollChange = 0f;
+        private float maxRollAngle = 45f;
+        private bool boost;
+
+
+
+        /// <summary>
+        /// Called when the agent is first initialized　エージェントの初期位置や初期状態の設定
+        /// </summary>
+        public override void Initialize()
+        {
+            area = GetComponentInParent<AircraftArea>();
+            rigidbody = GetComponent<Rigidbody>();
+            trail = GetComponent<TrailRenderer>();
+
+            // Override the max step set in the inspector
+            // Max 5000 steps if training, infinite steps if racing
+            //もしtrainigmodeがtrueであれば5000が代入　5000に達したら終了
+            MaxStep = area.trainingMode ? 5000 : 0;
+
+        }
+
+
+        public override void OnEpisodeBegin()
+        {
+            // Reset the velocity, position, and orientation
+            rigidbody.linearVelocity = Vector3.zero;
+            rigidbody.angularVelocity = Vector3.zero;
+            trail.emitting = false;
+        }
+
+
+
+
+        /// <summary>
+        /// Read action inputs from vectorAction 決定→行動         
+        /// </summary>
+        /// <param name = "vectorAction" > The chosen actions</param>
+        ///ビデオではfloatとしていたが、引数の書き方はこれでないとoverrideできない      
+
+        public override void OnActionReceived(ActionBuffers vectorAction)
+        {
+            if (frozen) return;
+
+
+            // vectorAction
+            // Read values for pitch and yaw
+            pitchChange = vectorAction.ContinuousActions[0]; // up or none
+            if (pitchChange == 2) pitchChange = -1f; // down
+            yawChange = vectorAction.ContinuousActions[1]; // turn right or none
+            if (yawChange == 2) yawChange = -1f; // turn left
+
+            // Read value for boost and enable/disable trail renderer
+            boost = vectorAction.ContinuousActions[2] == 1;
+            if (boost && !trail.emitting) trail.Clear();
+            trail.emitting = boost;
+
+            ProcessMovement();
+
+            // Small negative reward every step
+            AddReward(-1f / MaxStep);
+
+            // Make sure we haven't run out of time if training タイムアウトしてしまったらマイナス
+            if (StepCount > nextStepTimeout)
+            {
+                AddReward(-.5f);
+                EndEpisode();
+            }
+
+            //nextcheckpointへ到着したら今の場所が　localCheckpointDirへ
+            Vector3 localCheckpointDir = VectorToNextCheckpoint();
+            //もし次のチェックポイントへのベクトルが小さかったらチェックポイント到達
+            if (localCheckpointDir.magnitude < Academy.Instance.EnvironmentParameters.GetWithDefault("checkpoint_radius", 0f))
+            {
+                GotCheckpoint();
+            }
+        }
+
+
+        /// <summary>
+        /// Gets a vector to the next checkpoint the agent needs to fly through　
+        /// </summary>
+        /// <returns>A local-space vector</returns>
+        private Vector3 VectorToNextCheckpoint()
+        {
+            Vector3 nextCheckpointDir = area.Checkpoints[NextCheckpointIndex].transform.position - transform.position;
+            Vector3 localCheckpointDir = transform.InverseTransformDirection(nextCheckpointDir);
+            return localCheckpointDir;
+        }
+
+        /// <summary>
+        /// Called when the agent flies through the correct checkpoint     
+        /// </summary>
+        private void GotCheckpoint()
+        {
+            // Next checkpoint reached, update
+            NextCheckpointIndex = (NextCheckpointIndex + 1) % area.Checkpoints.Count;
+
+            if (area.trainingMode)
+            {
+                AddReward(.5f);
+                nextStepTimeout = StepCount + stepTimeout;
+            }
+        }
+
+
+
+
+
+
+
+
+
+        /// <summary>
+        /// Calculate and apply movement
+        /// </summary>
+
+        private void ProcessMovement()
+        {
+
+            // Calculate boost
+            float boostModifier = boost ? boostMultiplier : 1f;
+
+            // Apply forward thrust
+            rigidbody.AddForce(transform.forward * thrust * boostModifier, ForceMode.Force);
+
+            // Get the current rotation
+            Vector3 curRot = transform.rotation.eulerAngles;
+
+            // Calculate the roll angle (between -180 and 180)
+            float rollAngle = curRot.z > 180f ? curRot.z - 360f : curRot.z;
+            if (yawChange == 0f)
+            {
+            // Not turning; smoothly roll toward center
+                rollChange = -rollAngle / maxRollAngle;
+            }
+            else
+            {
+                // Turning; roll in opposite direction of turn
+                rollChange = -yawChange;
+            }
+
+            //culculate smooth deltas
+            smoothPitchChange = Mathf.MoveTowards(smoothPitchChange, pitchChange, 2f * Time.fixedDeltaTime);
+            smoothYawChange = Mathf.MoveTowards(smoothYawChange, yawChange, 2f * Time.fixedDeltaTime);
+            smoothRollChange = Mathf.MoveTowards(smoothRollChange, rollChange, 2f * Time.fixedDeltaTime);
+
+            // Calculate new pitch, yaw, and roll. Clamp pitch and roll.
+            float pitch = curRot.x + smoothPitchChange * Time.fixedDeltaTime * pitchSpeed;
+            if (pitch > 180f) pitch -= 360f;
+            pitch = Mathf.Clamp(pitch, -maxPitchAngle, maxPitchAngle);
+
+            float yaw = curRot.y + smoothYawChange * Time.fixedDeltaTime * yawSpeed;
+
+            float roll = curRot.z + smoothRollChange * Time.fixedDeltaTime * rollSpeed;
+            if (roll > 180f) roll -= 360f;
+            roll = Mathf.Clamp(roll, -maxRollAngle, maxRollAngle);
+
+            // Set the new rotation
+            transform.rotation = Quaternion.Euler(pitch, yaw, roll);
+
+
+        }
+
+
+
+    }
+}
+
+
+
